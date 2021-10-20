b2f314eb869bbef9f0edcb14d6bc45bef276946c
ThaiA12 <92841249+ThaiA12@users.noreply.github.com>
2021-10-20 12:46:18 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const  CONFIG  =  {
+    introTitle : 'Chào các bạn nữ 12A12 nha' ,
+    introDesc : ' Phiên bản hôm nay là ngày 20-10 nên con trai
+    lớp 12A12 có món quà tặng cho các cậu
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương` ,
+    btnIntro : 'hihi' ,
+    tiêu đề : 'phụ nữ hạnh phúc' s  ngày ' ,
+    desc : 'Các cậu có muốn nhận quà không? ' ,
+    btn Có : 'Không: 3' ,
+    btnNo : 'Có :))' ,
+    question : 'Trên thế giới hơn 7 tỉ người nhưng nụ cười của các bạn nữ A12 là xinh nhất' ,
+    btnReply : 'Tặng các bạn' ,
+    reply : 'tớ chỉ muốn nói là' ,
+    mess : 'Các cậu luôn là những bông hoa trong mắt tớ và Chúc các bạn luôn đẹp, đạt được ước mơ đậu vào trường mà mình mơ ước' ,
+    messDesc : 'Mong rằng bọn mình sẽ sớm gặp nhau để làm lẩu nhó' ,
+    btnAccept : 'Okiiiii nha, haha' ,
+    messLink : 'https://www.facebook.com/profile.php?id=100016275974205' 
+}
